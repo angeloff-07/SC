922482cfcef78cd5f7f72046af3b2e4f9130c75f
angeloff-07 <jorge.angeloff@mi.unc.edu.ar>
2024-08-29 11:09:33 -0300

    actualizacion del repo

--- Matlab/tarea3_angeloff_jorge.m
+++ Matlab/tarea3_angeloff_jorge.m
@@ -48,9 +48,9 @@ step(W_V,1)
 % pzmap(W_V);
 
 %% Ejercicio 4
+close all; clear all; clc
+
 syms  XA XS W s
-
-
 
 MA = 500; % Masa del automóvil
 MS = 50;  % Masa de la suspensión
@@ -58,8 +58,8 @@ BA = 80;  % Amortiguador
 KR = 10;  % Resorte entre automóvil y suspensión
 KC = 50;  % Resorte de la cubierta
 
-eq1 = MA * s^2 * XA == BA * (XS - XA) + KR * (XS - XA);
-eq2 = MS * s^2 * XS == BA * (XA - XS) + KR * (XA - XS) + KC * (W - XS);
+eq1 = (MA * s^2) * XA == BA * s * (XS - XA) + KR * (XS - XA);
+eq2 = (MS * s^2) * XS == BA * s * (XA - XS) + KR * (XA - XS) + KC * (W - XS);
 
 % Resolver el sistema para XA y XS
 [XA_sol, XS_sol] = solve([eq1, eq2], [XA, XS]);
@@ -67,5 +67,184 @@ eq2 = MS * s^2 * XS == BA * (XA - XS) + KR * (XA - XS) + KC * (W - XS);
 % Calcular la función de transferencia Xa/W
 Xa_W = simplify(XA_sol / W)
 pretty(Xa_W)
+%% 
+close all; clear all; clc
+s=tf('s');
+
+num = [ 8 , 1 ];
+den = [ 50, 88, 61, 8, 1];
+
+Xa_W=tf(num, den)
+
+step(Xa_W, 200);grid
+title('Respuesta al escalón del sistema de amortiguación')
+xlabel('Tiempo [s]'); ylabel('Amplitud [m]')
+
+%% Ejercicio de clase teorica
+close all; clear all; clc
+
+syms s M1 M2 B1 B2 B3 K F Y1 Y2
+
+eq1 = ( M2 * (s^2) * Y2 ) + ( B3 * s *Y2) + ( B2 * s * ( Y2 - Y1 ) ) + ( K * ( Y2 - Y1)) == 0;
+eq2 = ( M1 * (s^2) * Y1 ) + ( B1 * s *Y1) + ( B2 * s * ( Y2 - Y1 ) ) + ( K * ( Y2 - Y1)) == F;
+
+[Y1_SOL, Y2_SOL] =solve ([eq1, eq2], [Y1, Y2]);
+
+Y1_F = simplify(Y1_SOL/F);
+pretty(Y1_F)
+
+Y2_F = simplify(Y2_SOL/F);
+pretty(Y2_F)
 
 
+%% ejercicio 5
+close all; clear all; clc
+
+syms v ra i la kb wm jm wm ki bm s
+
+%suponiendo tl=0 condicion incial nula
+
+eq1 = v == ra * i + la * s * i +kb * wm;
+eq2 = jm * s *wm == ki * i - bm * wm;
+
+[w_sol, i_sol] = solve([eq1, eq2],[wm, (i)]);
+
+w_v = simplify(w_sol/v);
+
+pretty(w_v)
+
+theta_v = w_v * s;
+
+%% 
+close all; clear all; clc
+
+syms v ra i la kb wm jm wm ki bm s tl
+
+%suponiendo tl=0 condicion incial nula
+
+eq1 = 0 == ra * i + la * s * i +kb * wm;
+eq2 = jm * s *wm == ki * i - bm * wm - tl;
+
+[w_sol, i_sol] = solve([eq1, eq2],[wm, (i)]);
+
+w_tl = simplify(w_sol/tl);
+
+disp('w_tl = ')
+
+pretty(w_tl)
+
+%% 
+
+close all; clear all; clc
+
+s = tf('s');
+
+ra = 5.8; 
+la = 135.e-6;
+kb = 14.48e-3; 
+jm = 1.6e-2;
+ki = 14.48e-3;
+bm = 10e-5;
+
+
+G = ki / (( bm * ra ) + ( kb * ki ) + ( jm * la * (s^2) ) + (bm * la * s) + (jm * ra * s))
+
+step(12*G, 1000);
+
+%% Ejercicio 7
+
+close all; clear all; clc
+
+s = tf('s');
+
+ra = 5.8; 
+la = 135.e-6;
+kb = 14.48e-3; 
+jm = 1.6e-2;
+ki = 14.48e-3;
+bm = 10e-5;
+
+
+tm = ( ra * jm ) / ((ra * bm) + ( ki * kb ));
+
+G1 = 1 / ((tm * s) + 1 )
+G2 = 1 / (((tm/2)*s )+1) %% llega mas rapido al valor de regimen
+
+step(G1, G2)
+
+%% Ejercicio 6
+
+close all; clear all; clc
+
+syms s m1 x1 b1 u k1 k2 x2 m2 
+
+eq1 = m1 * s^2 *x1 == b1 * ( s * u - s * x1 ) + k1 * ( u - x1 ) +k2 * ( x2 - x1);
+eq2 = m2 * s^2 * x2 == k2 * ( x1 - x2 );
+
+[x1_sol, x2_sol] = solve ( [ eq1, eq2 ],[ x1 , x2 ]);
+
+x1_u = simplify(x1_sol/u);
+x2_u = simplify(x2_sol/u);
+
+x1_u = collect(x1_u, s); %%para ordenar las potencias
+x2_u = collect(x2_u, s);
+
+disp('x1_u = ')
+pretty(x1_u)
+
+disp('x2_u = ')
+pretty(x2_u)
+
+%% Ejercicio 8
+close all; clear all; clc
+
+syms m M i s theta l g x f b
+
+eq1 = i * s^2 * theta + m * l^2 * s^2 * theta == m * g * l * theta + m * l * s^2 * x;
+eq2 = (M + m) * s^2 * x == f - b * s *x + m * l * s^2 * theta;
+
+[theta_sol, x_sol] = solve ([eq1, eq2],[theta, x]);
+
+theta_f = simplify(theta_sol / f);
+
+theta_f = collect(theta_f, s);
+
+disp('theta_f = ')
+pretty(theta_f)
+
+%% Ejercicio 9
+close all; clear all; clc
+
+syms m s theta c g tm
+
+syms r i v l kb theta_m j ka b 
+
+eq1 = m * s^2 * theta + c * s * theta  + g * theta == tm;
+
+[theta_sol] = solve([eq1], [theta]);
+theta_sol = simplify(theta_sol)
+
+theta_tm = simplify(theta_sol / tm);
+theta_tm = collect(theta_tm, s);
+disp('theta_tm = ')
+pretty(theta_tm)
+
+tm = ka * i;
+
+eq2 = v == r * i +l * s * i + kb * s * theta_m;
+
+eq3 = j * s^2 * theta_m == ka * i - b * s * theta_m;
+
+[i_sol, theta_m_sol] = solve([eq2, eq3],[i, theta_m]);
+
+tm_v = simplify((ka * i_sol) / v);
+tm_v = collect(tm_v, s);
+disp('tm_v = ')
+pretty(tm_v)
+
+theta_v = theta_tm * tm_v;
+theta_v = collect(theta_v, s);
+disp('theta_v = ')
+pretty(theta_v)
+
+%%
